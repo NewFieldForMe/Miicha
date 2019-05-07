f0e03b001cb12fc8f89190971b5dcb31c71559cc
RyoYamada <newfield1001@gmail.com>
2019-05-07 15:22:17 +0900

    ホーム画面: Firestoreのデータをリストで仮表示

--- lib/screen/home/home_page.dart
+++ lib/screen/home/home_page.dart
@@ -5,6 +5,7 @@ import 'package:miicha_app/screen/home/home_drawer.dart';
 import 'package:miicha_app/screen/post/post_page.dart';
 import 'package:miicha_app/bloc/authentication_bloc.dart';
 import 'package:bloc_provider/bloc_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key,}) : super(key: key);
@@ -38,7 +39,30 @@ class _HomePageState extends State<HomePage> {
             }));
           },
           child: const Icon(Icons.add),
-      )
+      ),
+      body: _buildArticleList(),
+    );
+  }
+
+  Widget _buildArticleList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('articles').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting: return const Text('読み込み中...');
+          default:
+          return ListView(
+            children: snapshot.data.documents.map( (document) {
+              return ListTile(
+                title: Text(document['message'] as String),
+              );
+            }).toList(),
+          );
+        }
+      }
     );
   }
 }
