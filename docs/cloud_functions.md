86e54cbeb19e191424b2a66154019924268bb76e
RyoYamada <newfield1001@gmail.com>
2019-05-21 19:01:41 +0900

    CloudFunctions: ユーザーの投稿をフックして、Rootにタイムライン用データを追加するように修正

--- docs/cloud_functions.md
+++ docs/cloud_functions.md
@@ -1,12 +1,19 @@
 # Cloud Functionsについて
 ## 開発環境
-node: v8.15.0
-firebase-tools: v6.10.0
+- node: v8.15.0
+- firebase-tools: v6.10.0
+- TypeScriptを使用する
+
 [参考](https://cloud.google.com/functions/docs/concepts/nodejs-8-runtime)
 [Cloud Functionsのスタートガイド](https://firebase.google.com/docs/functions/get-started?authuser=0)
 
-### 環境構築
+### 環境構築手順
 1. `nodebrew`でnodeをインストール(nodeのバージョン管理ツールはお好みで)
 2. `npm install -g firebase-tools`を実行する
 
 ## Cloud Functionsのプロジェクトを作る
+1. `firebase init functions`
+
+## Deploy
+1. `cd functions`
+2. `firebase deploy --only functions`
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,29 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+const firestore = admin.firestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+interface Article {
+    readonly message: String;
+    readonly createDateTime: Date;
+}
+
+interface RootArticle extends Article {
+    authorRef?: FirebaseFirestore.DocumentReference;
+}
+
+export const onUsersArticleCreate = functions.firestore.document('users/{userId}/articles/{articleId}').onCreate(async (snapshot, context) => {
+    await copyToRootWithUsersArticle(snapshot, context);
+});
+
+export const onUsersArticleUpdate = functions.firestore.document('users/{userId}/articles/{articleId}').onUpdate(async (change, context) => {
+    await copyToRootWithUsersArticle(change.after, context);
+});
+
+async function copyToRootWithUsersArticle(snapshot: FirebaseFirestore.DocumentSnapshot, context: functions.EventContext) {
+    const articleId = snapshot.id;
+    const userId = context.params.userId;
+    const article = snapshot.data() as RootArticle;
+    article.authorRef = firestore.collection('users').doc(userId);
+    await firestore.collection('articles').doc(articleId).set(article, { merge: true });
+}
